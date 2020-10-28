72caa99830b9a59bd9a8f8671097928f47b73c4a
Valen <wang_wenl@worksap.co.jp>
2020-10-28 19:10:45 +0800

    implement post page content using markdown-it

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -20,10 +20,10 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="post-preview" v-for="{ node } in $page.posts.edges" :key="node.id">
-            <a href="post.html">
+            <g-link :to="`/post/${node.id}`">
               <h2 class="post-title">{{ node.title }}</h2>
               <!-- <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3> -->
-            </a>
+            </g-link>
             <p class="post-meta">
               Posted by
               <a href="#">{{ `Valen Wang` }}</a>
